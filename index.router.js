de63c9a8d1c5a59085311820ab38eea64882eed2
MohammedHElmasry <01122437793rr@gmail.com>
2024-05-13 05:23:51 +0200

    ref

--- index.router.js
+++ index.router.js
@@ -3,12 +3,13 @@ import learnguide from "./src/modules/learnguide/learnguide.router.js";
 import user from "./src/modules/user/user.router.js";
 import { asyncHandler } from "./src/utils/errorhandling.js";
 import { handelerror } from "./src/utils/errorhandling.js";
-
+import cors from "cors";
 export const bootstrap = (app, express) => {
+  app.use(cors());
   app.use(express.json());
   app.use("/auth", auth);
   app.use("/user", user);
-  app.use("/learnguide",learnguide)
+  app.use("/learnguide", learnguide);
   app.use(asyncHandler);
   app.use(handelerror);
 };
