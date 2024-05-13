e050d3ad3425f8b30689a0753640348bf245f7b8
MohammedHElmasry <01122437793rr@gmail.com>
2024-05-13 05:35:56 +0200

    ref

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { connect } from "./db/connection.js";
 import { bootstrap } from "./index.router.js";
 
 const app = express();
-const port = 5000;
+const port= process.env.PORT || 5000
 connect();
 bootstrap(app, express);
 app.listen(port, () => {
